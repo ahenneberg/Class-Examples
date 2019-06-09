9be40a65da86706ce62adcfcfa5dc70140b19673
Austin Henneberg <51038369+ahenneberg@users.noreply.github.com>
2019-06-09 16:24:54 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+/* Disclaimer: The examples and comments from this program are from
+   C#7 in a Nutshell and are written for learning purposes only. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Example
+{
+    /* A class is the most common kind of reference type. The simplest possible class
+     declaration is as follows: */
+     class YourClassName
+    {
+
+    }
+    /* A more complex class optionally has the following: 
+     
+     Precediing the keyword class:  Attributes and class modifiers. The non-nested class   
+                                    modifiers are public, internal, abstract, sealed,
+                                    static, unsafe, and partial
+
+     Following YourClassName:       Generic Type Parameters, a base class, and interfaces
+
+     Within the Braces:             Class membes (these are methods, properties, indexers,
+                                    events, fields, constructors, overloaded operators,
+                                    nested types, and a finalizer) */
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
